706e8f0b86e21a0d1fdc34bf171128587f63dc45
DeJuanP <ninerperry88@hotmail.com>
2025-03-27 21:40:12 -0400

    March 27

--- codewars/march2025.js
+++ codewars/march2025.js
@@ -22,3 +22,10 @@ function isIsogram(word){
       }
     return true;
   }
+
+  //Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+  function descendingOrder(n){
+    return Number(n.toString().split('').sort((a,b) => b-a).join(''));
+  }
+  
