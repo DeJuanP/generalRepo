74d92f8d7e02d32f8a5f8d6be33d39af84fdbac8
DeJuanP <ninerperry88@hotmail.com>
2025-03-31 16:55:31 -0400

    March 31

--- codewars/march2025.js
+++ codewars/march2025.js
@@ -58,3 +58,19 @@ function isIsogram(word){
     return false;
   
   }
+
+//Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+  function longest(s1, s2) {
+    let output = [];
+    let combi = s1.concat(s2);
+    let array = combi.split('').sort();
+ 
+    for (let i = 0; i <= array.length; i++) {
+        console.log(array[i]);//test
+        if (!output.includes(array[i]) ) {
+              output.push(array[i]);
+        }
+    }
+    return output.join('');
+}
