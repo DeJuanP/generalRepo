a4f5426a973f79b91d4b49dd4888ae58f432f246
DeJuanP <ninerperry88@hotmail.com>
2025-04-04 11:11:13 -0400

    April 4th

--- codewars/april2025.js
+++ codewars/april2025.js
@@ -40,4 +40,31 @@ function calculateYears(principal, interest, tax, desired) {
     }
     return years;
 }
+
+//Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively. Each function returns one number.
+
+// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
+
+var min = function(list){
+  
+    function compareNumbers(a, b) {
+    return a - b;
+  }
+   let newArray = list.sort(compareNumbers) 
+      return newArray[0];
+  }
+  
+  var max = function(list){
+    
+      function compareNumbers1(a, b) {
+    return a - b;
+  }
+   let newArray1 = list.sort(compareNumbers1) 
+   let lastNumber = newArray1.pop();
+    return lastNumber
+  
+  }
   
