ba92b2ce0c946e9fefdf3ff074c79eff94775160
DeJuanP <ninerperry88@hotmail.com>
2025-04-11 22:43:16 -0400

    April 11th

--- codewars/april2025.js
+++ codewars/april2025.js
@@ -126,4 +126,12 @@ function abbrevName(name){
 }
 
 
+//Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
 
+//Examples:
+//"This is an example!" ==> "sihT si na !elpmaxe"
+//"double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+  return str.split(' ').map( str => str.split('').reverse().join('') ).join(' ');
+}
